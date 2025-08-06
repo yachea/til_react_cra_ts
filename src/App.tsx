2afd1634f30b77f4ad17e1eb0310fe21beac4602
yachea <dev.yachea@gmail.com>
2025-08-06 10:53:06 +0900

    [docs] 컴포넌트의 이해

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
-function App() {
-  const unused = 1;
-  return <button tabIndex={0}>App</button>;
-}
+type DemoProps = {
+  name: string;
+  age: string;
+};
+const Demo = ({ name, age }: DemoProps): JSX.Element => {
+  return (
+    <div>
+      {name}이구요, {age}살 입니다.
+    </div>
+  );
+};
+
+type SampleProps = {
+  children?: React.ReactNode;
+  title: string;
+};
+const Sample = ({ children, title }: SampleProps): JSX.Element => {
+  return (
+    <div>
+      <h2>Sample</h2>
+      <div>{title}</div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Sample title="Props 전달된 title 입니다."></Sample>
+      <p>Children 입니다.</p>
+    </div>
+  );
+};
 
 export default App;
